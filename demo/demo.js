1b2cc60e2beb4d25c29455c0624229394b1de573
Mitchell Burton <mitch.b45@gmail.com>
2018-04-19 18:44:17 -0700

    somewhat working dialogue?!?!?!

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-import Tile from '../lib/base/tile.js';
+import { DialogueTile } from '../lib/base/tile.js';
 import VCGame from '../lib/main.js';
 
 /* What I want this to look like */
@@ -6,7 +6,7 @@ import VCGame from '../lib/main.js';
 let mainSprite = new Image();
 let bgImage = new Image();
 mainSprite.src = './sprites/main.png';
-bgImage.src = './backgrounds/vc.png';
+bgImage.src = './backgrounds/vc-inset.png';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let demoGame = new VCGame({
     scaling: 3,
     tileSize: 16,
-    startTile: [18, 20],
+    startTile: [5, 4],
   });
 
   demoGame.addMainCharacter({
@@ -26,8 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
   demoGame.addBackground({
     canvas: bgCanvas,
     image: bgImage,
-    width: 640,
-    height: 576,
+    width: 192,
+    height: 128,
   });
 
   /*for (let i = 0; i < 40 * 36 - 1; i++) {
@@ -38,26 +38,78 @@ document.addEventListener('DOMContentLoaded', () => {
     demoGame.addTile(tile);
   }*/
 
-  let tileMap = [{
-    type: 'range',
-    start: [0, 0],
-    end: [20, 36],
-    options: {
-      walkable: true,
-      interactible: false,
+  let tileMap = [
+    {
+      type: 'range',
+      start: [0, 0],
+      end: [12, 8],
+      options: {
+        walkable: true,
+        interactible: false,
+      },
     },
-  },
-  {
-    type: 'range',
-    start: [21, 0],
-    end: [40, 36],
-    options: {
-      walkable: true,
-      interactible: true,
+    {
+      type: 'range',
+      start: [0, 0],
+      end: [3, 3],
+      options: {
+        walkable: false,
+        interactible: false,
+      },
+    },
+    {
+      type: 'range',
+      start: [3, 0],
+      end: [4, 2],
+      options: {
+        walkable: false,
+        interactible: false,
+      }
+    },
+    {
+      type: 'range',
+      start: [7, 0],
+      end: [11, 2],
+      options: {
+        walkable: false,
+        interactible: false,
+      }
+    },
+    {
+      type: 'range',
+      start: [7, 3],
+      end: [11, 4],
+      options: {
+        walkable: false,
+        interactible: false,
+      }
     }
-  }];
+  ];
 
   demoGame.addTile(tileMap);
+
+  let dialogueBg = new Image();
+  dialogueBg.src = './dialogues/background.png';
+  let dialogueTile = new DialogueTile({
+    walkable: false,
+    interactible: true,
+    dialogueOptions: {
+      dialogue: 'Welcome to the demo!',
+      dialogueBackground: dialogueBg,
+      letterMap: dialogueBg,
+      context: bgCanvas.getContext('2d'),
+      width: 160,
+      height: 48,
+      xLoc: 0,
+      yLoc: (144 - 48) * 3,
+      scaling: 3,
+    },
+  });
+
+  dialogueTile.x = 4;
+  dialogueTile.y = 3;
+
+  demoGame.addTile(dialogueTile)
 
   demoGame.start();
 
